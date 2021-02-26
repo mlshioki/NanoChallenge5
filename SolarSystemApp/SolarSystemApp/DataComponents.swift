27b8d0d78095aa0a41bb4e83791b5e398b903fb2
Sky10line <61299872+Sky10line@users.noreply.github.com>
2021-02-26 15:09:48 -0300

    Detailed screen moon completa

--- SolarSystemApp/SolarSystemApp/DataComponents.swift
+++ SolarSystemApp/SolarSystemApp/DataComponents.swift
@@ -7,9 +7,9 @@
 
 import Foundation
 
-protocol DataFuncs {
-    func getData(url: String) -> Body
-}
+//protocol DataFuncs {
+//    func getData(url: String) -> Body
+//}
 
 struct Body: Codable{
     let englishName: String?
@@ -21,26 +21,26 @@ struct Body: Codable{
 
 //usar a func abaixo quando for delegar para outra classe
 
-//func getData(from url: String) -> Body{
-//    var res: Body!
-//
-//    let group = DispatchGroup()
-//    group.enter()
-//    URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { data, response, error in
-//        guard let data = data, error == nil else {
-//            print("error getData")
-//            return
-//        }
-//        var result: Body
-//        do {
-//            result = try JSONDecoder().decode(Body.self, from: data)
-//            res = result
-//        }
-//        catch {
-//            print("error convert \(error.localizedDescription)")
-//        }
-//        group.leave()
-//    }).resume()
-//    group.wait()
-//    return res
-//}
+func getData(from body: String) -> Body{
+    var res: Body!
+    let url = "https://api.le-systeme-solaire.net/rest/bodies/\(body)"
+    let group = DispatchGroup()
+    group.enter()
+    URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { data, response, error in
+        guard let data = data, error == nil else {
+            print("error getData")
+            return
+        }
+        var result: Body
+        do {
+            result = try JSONDecoder().decode(Body.self, from: data)
+            res = result
+        }
+        catch {
+            print("error convert \(error.localizedDescription)")
+        }
+        group.leave()
+    }).resume()
+    group.wait()
+    return res
+}
--- SolarSystemApp/SolarSystemApp/DetailedScreenMoonViewController.swift
+++ SolarSystemApp/SolarSystemApp/DetailedScreenMoonViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class DetailedScreenMoonViewController: UIViewController {
 
-    var id: String?
+    var id: String = ""
     
     @IBOutlet var moonImage: UIImageView!
     @IBOutlet weak var moonName: UILabel!
@@ -21,12 +21,14 @@ class DetailedScreenMoonViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        moonImage.image = UIImage(named: "")
-        moonName.text = ""
-        size.text = ""
-        gravity.text = ""
-        discoveryDate.text = ""
-        alternativeName.text = ""
+        let data = getData(from: id)
+        
+        moonImage.image = UIImage(named: id)
+        moonName.text = "\(data.englishName ?? "???")"
+        size.text = "Size (Mean Radius): \(data.meanRadius ?? 0)"
+        gravity.text = "Gravity: \(data.gravity ?? 0)"
+        discoveryDate.text = "Discovery Date: \(data.discoveryDate ?? "???")"
+        alternativeName.text = "Alternative Name: \(data.alternativeName ?? "???")"
         
     }
 }
